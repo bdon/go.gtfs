e654fd929e2122b4df66d5fb121df09284cd7f63
Brandon Liu <bdon@bdon.org>
2014-04-27 10:50:25 -0700

    add basic gtfs code.

---
+++ gtfs.go
@@ -0,0 +1,85 @@
+package gtfs
+
+import (
+  "path"
+  "os"
+  "encoding/csv"
+  "io"
+  "strings"
+)
+
+type Feed struct {
+  Dir string
+  Routes map[string]Route
+}
+
+type Route struct {
+  Id string
+  ShortName string
+  Trips []Trip
+}
+
+type Trip struct {
+  Id string
+}
+
+// main utility function for reading GTFS files
+func (feed *Feed) readCsv (filename string, f func([]string)) {
+	file, err := os.Open(path.Join(feed.Dir,filename))
+	if err != nil {
+    p(err)
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	reader.TrailingComma = true
+  firstLineSeen := false
+	for {
+		record, err := reader.Read()
+    if !firstLineSeen {
+      firstLineSeen = true
+      continue
+    }
+		if err == io.EOF {
+			break
+		} else if err != nil {
+		} else {
+      f(record)
+    }
+	}
+}
+
+func Load(feed_path string) Feed {
+  f := Feed{Dir:feed_path}
+  f.Routes = make(map[string]Route)
+
+  f.readCsv("routes.txt", func(s []string) {
+    rsn := strings.TrimSpace(s[2])
+    id := strings.TrimSpace(s[0])
+    f.Routes[id] = Route{Id:id,ShortName:rsn}
+  })
+
+  f.readCsv("trips.txt", func(s []string) {
+    route_id := s[0]
+    route := f.Routes[route_id]
+    route.Trips = append(route.Trips, Trip{Id:"5"})
+    f.Routes[route_id] = route
+  })
+
+  return f
+}
+
+func (feed *Feed) RouteByShortName(shortName string) Route {
+   for _,v := range feed.Routes {
+      if v.ShortName == shortName {
+        return v
+      }
+   }
+   //TODO error here
+   return Route{}
+}
+
+// get All shapes for a route
+//
+func (route *Route) Shapes {
+
+}
---
+++ gtfs_test.go
@@ -0,0 +1,41 @@
+package gtfs
+
+import (
+  "testing"
+  "fmt"
+)
+
+func p(s interface{}) {
+  fmt.Println(s)
+}
+
+func TestLoadRoutes(t *testing.T) {
+  feed := Load("./fixtures")
+
+	if len(feed.Routes) < 1 {
+		t.Error("Feed should have at least one route")
+	}
+
+  res := feed.Routes["1093"].Id
+  if  res != "1093" {
+    t.Errorf("Feed should have route IDs, got %s", res)
+  }
+
+  res = feed.RouteByShortName("N").ShortName
+  if  res != "N" {
+    t.Errorf("Feed should be addressable by Route short name, got %s", res)
+  }
+}
+
+func TestTrips(t *testing.T) {
+  feed := Load("./fixtures")
+
+  res := len(feed.RouteByShortName("N").Trips)
+  if  res != 23 {
+    t.Errorf("Route should have all trips, got %d", res)
+  }
+}
+
+func TestShapes(t *testing.T) {
+
+}
